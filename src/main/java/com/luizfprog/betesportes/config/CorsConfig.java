a167da9d898e00000079c748ea5ac1b70b54424e
LuizFprog <luizgomes.prog@gmail.com>
2025-07-17 09:17:57 -0300

    fix: cors configuration

--- src/main/java/com/luizfprog/betesportes/config/CorsConfig.java
+++ src/main/java/com/luizfprog/betesportes/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")                        // ↰ agora cobre /teams, /users, etc.
-                        .allowedOrigins("*") // front
+                        .allowedOrigins("http://localhost:8081") // front
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
