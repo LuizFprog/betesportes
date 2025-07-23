f33be7ae7515d7b55fde8db4fea0e27c6b51ea3e
LuizFprog <luizgomes.prog@gmail.com>
2025-07-23 09:17:47 -0300

    fix: ticket repository query fix

--- src/main/java/com/luizfprog/betesportes/repository/TicketRepository.java
+++ src/main/java/com/luizfprog/betesportes/repository/TicketRepository.java
@@ -10,27 +10,30 @@ import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     @Query("""
-        select t
-        from Ticket t join t.matches m
-        where size(t.matches) = 1
-          and m.startTime > :now
-        """)
+    select t
+    from Ticket t join t.matches m
+    where m.startTime > :now
+    group by t
+    having count(m) = 1
+    """)
     List<Ticket> findUpcomingSingleMatchTickets(@Param("now") LocalDateTime now);
 
     @Query("""
-        select t
-        from Ticket t join t.matches m
-        where size(t.matches) = 1
-          and m.startTime <= :now
-          and m.estimatedEndTime >= :now
-        """)
+    select t
+    from Ticket t join t.matches m
+    where m.startTime <= :now and m.estimatedEndTime >= :now
+    group by t
+    having count(m) = 1
+    """)
     List<Ticket> findOngoingSingleMatchTickets(@Param("now") LocalDateTime now);
 
     @Query("""
-        select t
-        from Ticket t join t.matches m
-        where size(t.matches) = 1
-          and m.estimatedEndTime < :now
-        """)
+    select t
+    from Ticket t join t.matches m
+    where m.estimatedEndTime < :now
+    group by t
+    having count(m) = 1
+    """)
     List<Ticket> findFinishedSingleMatchTickets(@Param("now") LocalDateTime now);
+
 }
