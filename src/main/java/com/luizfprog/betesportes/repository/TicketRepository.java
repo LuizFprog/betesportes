054abb471a6a11664791ae9a99db6fec49ec5e07
LuizFprog <luizgomes.prog@gmail.com>
2025-07-23 09:26:04 -0300

    fix: query

--- src/main/java/com/luizfprog/betesportes/repository/TicketRepository.java
+++ src/main/java/com/luizfprog/betesportes/repository/TicketRepository.java
@@ -11,29 +11,36 @@ import java.util.List;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     @Query("""
     select t
-    from Ticket t join t.matches m
+    from Ticket t
+    join t.matches b
+    join b.match m
     where m.startTime > :now
     group by t
-    having count(m) = 1
+    having count(b) = 1
     """)
     List<Ticket> findUpcomingSingleMatchTickets(@Param("now") LocalDateTime now);
 
     @Query("""
     select t
-    from Ticket t join t.matches m
+    from Ticket t
+    join t.matches b
+    join b.match m
     where m.startTime <= :now and m.estimatedEndTime >= :now
     group by t
-    having count(m) = 1
+    having count(b) = 1
     """)
     List<Ticket> findOngoingSingleMatchTickets(@Param("now") LocalDateTime now);
 
     @Query("""
     select t
-    from Ticket t join t.matches m
+    from Ticket t
+    join t.matches b
+    join b.match m
     where m.estimatedEndTime < :now
     group by t
-    having count(m) = 1
+    having count(b) = 1
     """)
     List<Ticket> findFinishedSingleMatchTickets(@Param("now") LocalDateTime now);
+
 
 }
